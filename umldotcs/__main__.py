8737d0e5cb376e8371401c5ec718e9bc5e95ad90
Kristian Thy <thy@42.dk>
2020-02-10 11:11:45 +0100

    fix: add import guard, shebang

--- umldotcs/__main__.py
+++ umldotcs/__main__.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """Main entrypoint for the uml.cs CLI."""
 
-from umldotcs.cli import create_uml
+try:
+    from cli import create_uml
+except (ImportError, ModuleNotFoundError):
+    from umldotcs.cli import create_uml
 
 if __name__ == "__main__":
     create_uml()  # pylint: disable=no-value-for-parameter
