709aa9ba228625dc55bcd6bec8100ea0a8f0b440
Kristian Thy <thy@42.dk>
2020-02-11 12:55:31 +0100

    chore: add pragmas

--- umldotcs/__main__.py
+++ umldotcs/__main__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Main entrypoint for the uml.cs CLI."""
+"""Main entrypoint for the uml.cs CLI."""                                         # pragma: no cover
 
-try:
+try:                                                                              # pragma: no cover
     from cli import create_uml
-except (ImportError, ModuleNotFoundError):
+except (ImportError, ModuleNotFoundError):                                        # pragma: no cover
     from umldotcs.cli import create_uml
 
-if __name__ == "__main__":
-    create_uml()  # pylint: disable=no-value-for-parameter
+if __name__ == "__main__":                                                        # pragma: no cover
+    create_uml()                                            # pylint: disable=no-value-for-parameter
