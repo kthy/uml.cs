5802c4965eb0711bc31a931bc0c516a037592354
Kristian Thy <thy@42.dk>
2020-02-13 13:25:24 +0100

    ci: try to fix broken tests on Travis

--- tests/test_creator.py
+++ tests/test_creator.py
@@ -42,7 +42,7 @@ def test_extract_object():
         ]
     )
     assert obj.__class__.__name__ == "UmlClass"
-    assert obj.access == Access.PUBLIC
+    assert obj.access.value == Access.PUBLIC.value
     assert obj.modifiers == [Modifier.STATIC]
     assert obj.name == "StaticKlass"
     assert obj.implements == ["ICanBeImplemented", "IComparable", "IEquatable_T_"]
