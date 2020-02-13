7f6f1d27a8ce058529bf1156fa84a313af790757
Kristian Thy <thy@42.dk>
2020-02-13 12:44:04 +0100

    fix: make code more testable

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,29 @@
 """Test the CLI."""
 
-from umldotcs.cli import NAMESPACES, exclude, zip_namespaces
+from umldotcs.cli import (
+    NAMESPACES,
+    RELATIONS,
+    exclude,
+    glob_files,
+    write_output,
+    zip_namespaces,
+    zip_relations,
+)
+
+
+def test_glob_files():
+    """Test cli.glob_files(dir)."""
+    base_path = "./tests/sln/"
+    assert glob_files(".") == [
+        base_path + f
+        for f in [
+            "Uml.Cs.App/Program.cs",
+            "Uml.Cs.Dll/ICanBeImplemented.cs",
+            "Uml.Cs.Dll/SubUmlCsDll.cs",
+            "Uml.Cs.Dll/UmlCsDll.cs",
+            "Uml.Cs.Dll/UmlEnum.cs",
+        ]
+    ]
 
 
 def test_exclude():
@@ -24,8 +47,14 @@ def test_exclude():
     assert not any([exclude(p) for p in should_be_included])
 
 
+def test_write_output():
+    """Test cli.write_output()."""
+    assert NAMESPACES == {}
+    assert write_output(None, None, None, None) == 0
+
+
 def test_zip_namespaces():
-    """Test cli.zip_namespaces."""
+    """Test cli.zip_namespaces()."""
     assert NAMESPACES == {}
     nsp_inner = {"Inner.Space": [1, 2, 3]}
     zip_namespaces(nsp_inner)
@@ -36,3 +65,11 @@ def test_zip_namespaces():
     zip_namespaces(nsp_outer2)
     nsp_merged = {"Inner.Space": [1, 2, 3], "Outer.Space": [4, 5, 6, 7, 8, 9]}
     assert NAMESPACES == nsp_merged
+    NAMESPACES == {}  # pylint: disable=pointless-statement
+
+
+def test_zip_relations():
+    """Test cli.zip_relations()."""
+    assert RELATIONS == []
+    zip_relations(["foo"])
+    assert RELATIONS == ["foo"]
--- umldotcs/cli.py
+++ umldotcs/cli.py
@@ -4,7 +4,7 @@
 from glob import glob
 from os.path import join
 from re import search
-from subprocess import run  # nosec
+from subprocess import CalledProcessError, run  # nosec
 
 import click
 
@@ -14,7 +14,7 @@ except (ImportError, ModuleNotFoundError):
     from umldotcs.creator import UmlCreator
 
 NAMESPACES = dict()
-RELATIONS = []
+RELATIONS = list()
 
 # TODO: add option for exclusions
 @click.command()
@@ -26,24 +26,37 @@ RELATIONS = []
 @click.option("-u", "--repo-url")
 def create_uml(directory, font, label, output_gv, output_svg, repo_url):
     """Process all .cs files in directory and its sub-directories."""
-    files = [f for f in glob(join(directory, "**", "*.cs"), recursive=True) if not exclude(f)]
-    print(f"Processing {files}")
+    files = glob_files(directory)
     for file_path in files:
-        uml_creator = UmlCreator(file_path, repo_url)
-        nsp, rel = uml_creator.process_file()
+        click.echo(f"Processing {click.format_filename(file_path)}")
+        nsp, rel = UmlCreator(file_path, repo_url).process_file()
         zip_namespaces(nsp)
-        RELATIONS.extend(rel)
-    if NAMESPACES:
-        UmlCreator.write_gv(output_gv, label, font, NAMESPACES, RELATIONS)
-        if output_svg:
-            run(["dot", "-Tsvg", "-o", output_svg, output_gv], check=False)
-    else:
-        print("NO CODE")
+        zip_relations(rel)
+    write_output(font, label, output_gv, output_svg)
+
+
+def glob_files(directory):
+    """Return list of non-excluded files in dir and its subdirs."""
+    return [f for f in glob(join(directory, "**", "*.cs"), recursive=True) if not exclude(f)]
 
 
 def exclude(path):
     """Return True if the path should be excluded."""
     return search(r"AssemblyInfo\.cs|Test\.cs|/(bin|obj)/(Debug|Release)/", path)
+
+
+def write_output(font, label, output_gv, output_svg):
+    """Write GraphViz file and optionally run dot to convert it to SVG."""
+    if NAMESPACES:
+        UmlCreator.write_gv(output_gv, label, font, NAMESPACES, RELATIONS)
+        if output_svg:
+            try:
+                run(["dot", "-Tsvg", "-o", output_svg, output_gv], check=True)
+            except CalledProcessError:
+                return 2
+    else:
+        click.secho("NO CODE", fg="bright_red", bold=True)
+    return 0
 
 
 def zip_namespaces(nsp):
@@ -53,3 +66,8 @@ def zip_namespaces(nsp):
             NAMESPACES[key].extend(val)
         else:
             NAMESPACES[key] = val
+
+
+def zip_relations(rel):
+    """Append relations to global list."""
+    RELATIONS.extend(rel)
